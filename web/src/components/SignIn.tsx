393cad70ba82bf8e9c2a1b6b8032a1dd18897285
erik-ferreira <erikdesouzagf@gmail.com>
2023-05-19 16:24:18 -0300

    separate components; link to sign in with github

---
+++ web/src/components/SignIn.tsx
@@ -0,0 +1,18 @@
+import { User } from "lucide-react"
+
+export function SignIn() {
+  return (
+    <a
+      href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
+      className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+    >
+      <div className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-400">
+        <User className="w-6 h-6 text-gray-500" />
+      </div>
+
+      <p className="max-w-[140px] text-sm leading-snug">
+        <span className="underlined">Crie sua conta</span> e salve suas memórias
+      </p>
+    </a>
+  )
+}
