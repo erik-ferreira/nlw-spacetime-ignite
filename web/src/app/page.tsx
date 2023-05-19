1989e46231e2e4b318df8ec6509a4c0ce26ee8c0
erik-ferreira <erikdesouzagf@gmail.com>
2023-05-19 17:54:31 -0300

    show profile user; complete auth web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,14 @@
+import { cookies } from "next/headers"
+
 import { Hero } from "@/components/Hero"
 import { SignIn } from "@/components/SignIn"
+import { Profile } from "@/components/Profile"
 import { Copyright } from "@/components/Copyright"
 import { EmptyMemories } from "@/components/EmptyMemories"
 
 export default function Home() {
+  const isAuthenticated = cookies().has("token")
+
   return (
     <main className="min-h-screen grid grid-cols-2">
       <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
@@ -13,10 +18,8 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        <SignIn />
-
+        {isAuthenticated ? <Profile /> : <SignIn />}
         <Hero />
-
         <Copyright />
       </div>
 
