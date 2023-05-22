9a9b43b19452d7d35ee005164f2fe20795500df8
erik-ferreira <erikdesouzagf@gmail.com>
2023-05-22 17:02:13 -0300

    logout; layout app; style and structure page new memory

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -5,6 +5,11 @@ import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamjuree,
 } from "next/font/google"
+import { cookies } from "next/headers"
+import { Hero } from "@/components/Hero"
+import { SignIn } from "@/components/SignIn"
+import { Profile } from "@/components/Profile"
+import { Copyright } from "@/components/Copyright"
 
 const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" })
 const baiJamJuree = BaiJamjuree({
@@ -20,12 +25,30 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has("token")
+
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
         className={`${roboto.variable} ${baiJamJuree.variable} font-sans text-gray-100 bg-gray-900`}
       >
-        {children}
+        <main className="min-h-screen grid grid-cols-2">
+          <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
+            {/* Blur */}
+            <div className="w-[526px] h-[288px] rounded-full bg-purple-700 absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2 blur-full" />
+
+            {/* Stripes */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            <Hero />
+            <Copyright />
+          </div>
+
+          <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,31 +1,5 @@
-import { cookies } from "next/headers"
-
-import { Hero } from "@/components/Hero"
-import { SignIn } from "@/components/SignIn"
-import { Profile } from "@/components/Profile"
-import { Copyright } from "@/components/Copyright"
 import { EmptyMemories } from "@/components/EmptyMemories"
 
 export default function Home() {
-  const isAuthenticated = cookies().has("token")
-
-  return (
-    <main className="min-h-screen grid grid-cols-2">
-      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
-        {/* Blur */}
-        <div className="w-[526px] h-[288px] rounded-full bg-purple-700 absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2 blur-full" />
-
-        {/* Stripes */}
-        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
-
-        {isAuthenticated ? <Profile /> : <SignIn />}
-        <Hero />
-        <Copyright />
-      </div>
-
-      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
-        <EmptyMemories />
-      </div>
-    </main>
-  )
+  return <EmptyMemories />
 }
